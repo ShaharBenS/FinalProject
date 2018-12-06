6a752e4e93617e8dc8bd6638fc6394e6feb1d7e6
Tomer Lev <levtom@post.bgu.ac.il>
2018-12-06 19:19:03 +0200

    Little Fixes.

--- schemas/UsersAndRole.js
+++ schemas/UsersAndRole.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 var usersAndRole = new Schema({
     roleName: {type:String, unique:true},
-    userID: [{type:Schema.Types.ObjectId, ref:'User'}],
+    userEmail: [{type:Schema.Types.ObjectId, ref:'User'}],
     children: [{type:Schema.Types.ObjectId, ref:'UsersAndRoles'}]
 });
 
