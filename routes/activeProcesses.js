e1dc656806cef09f68f2e7d656674ab0c1791111
Omri Attiya <omri206attiya@gmail.com>
2018-12-09 18:37:18 +0200

    create process structure is working

--- routes/activeProcesses.js
+++ routes/activeProcesses.js
@@ -3,16 +3,19 @@ let activeProcess = require('../controllers/processes/activeProcess');
 
 let router = express.Router();
 
-router.post('/activateProcess', function (req, res) {
-    let structure_name = req.body.structure_name;
-    let initial_stage = req.body.initial_stage;
-    let process_name = req.body.process_name;
-
-    activeProcess.activateProcess(structure_name,process_name,initial_stage, (err)=>{
-        if(err){
+router.post('/startProcess', function (req, res) {
+    /* let structure_name = req.body.structure_name;
+     let initial_stage = req.body.initial_stage;
+     let process_name = req.body.process_name;
+ */
+    let structure_name = "structure 1";
+    let process_name = "active process 1";
+    let username = "omri@post.bgu.ac.il";
+    activeProcess.startProcessByUsername(username, structure_name, process_name, (err) => {
+        if (err) {
             res.send(err);
         }
-        else{
+        else {
             res.send("Activated Successfully");
         }
     });
@@ -20,15 +23,15 @@ router.post('/activateProcess', function (req, res) {
 
 router.get('/getAllActiveProcessesByUser', function (req, res) {
     let user_name = req.body.user_name;
-    activeProcess.getAllActiveProcessesByUser(user_name,(array)=>{
-        res.render('MyActiveProcessesPage', {title: 'Express', table : array});
+    activeProcess.getAllActiveProcessesByUser(user_name, (array) => {
+        res.render('MyActiveProcessesPage', {title: 'Express', table: array});
     });
 });
 
 router.get('/getWaitingActiveProcessesByUser', function (req, res) {
     let user_name = req.body.user_name;
-    activeProcess.getWaitingActiveProcessesByUser(user_name,(array)=>{
-        res.render('MyWaitingProcessesPage', {title: 'Express', table : array});
+    activeProcess.getWaitingActiveProcessesByUser(user_name, (array) => {
+        res.render('MyWaitingProcessesPage', {title: 'Express', table: array});
     });
 });
 
