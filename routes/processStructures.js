9b0df81807c8bc0e6e6e3bc158d0103c6db69b56
Kuti1997 <kutigolberg@gmail.com>
2018-12-20 21:08:11 +0200

    Minor Fix

--- routes/processStructures.js
+++ routes/processStructures.js
@@ -5,24 +5,6 @@ let userControl = require('../controllers/UsersAndRoles');
 
 let router = express.Router();
 
-router.post('/addProcessStructure', function (req, res) {
-    let structure_name = req.body.structure_name;
-    let initials = req.body.initials;
-    let stages =  req.body.stages;
-    processStructure.addProcessStructure(structure_name, initials, stages, (err) => {
-        if (err)
-            res.send("fail");
-        else res.send("success");
-    });
-});
-
-router.post('/editProcessStructure', function (req, res) {
-    let structure_name = req.body.structure_name;
-    let initials = req.body.initials;
-    let stages = req.body.stages;
-    processStructure.editProcessStructure(structure_name, initials, stages, (result) => res.send(result));
-});
-
 router.post('/removeProcessStructure', function (req, res) {
     let structure_name = req.body.structure_name;
     processStructure.removeProcessStructure(structure_name, (result) => res.send(result));
