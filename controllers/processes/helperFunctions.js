4a716c4ba4b3ccec5f7488c9ea1e3f7e63be542b
Omri <omri206attiya@gmail.com>
2018-12-20 13:30:12 -0800

    spaces fix in code

--- controllers/processes/helperFunctions.js
+++ controllers/processes/helperFunctions.js
@@ -7,7 +7,7 @@ exports.getRoleID_by_username = function (username, callback) {
     UsersAndRole.find({userEmail: username}, (err, user) => {
         if (err) callback(err);
         else {
-            if (user.length === 0) callback(null,null);
+            if (user.length === 0) callback(null, null);
             else callback(null, user[0]._doc._id);
         }
     });
@@ -18,7 +18,7 @@ exports.getProcessStructure = function (processStructureName, callback) {
     ProcessStructure.find({structure_name: processStructureName}, (err, processStructure) => {
         if (err) callback(err);
         else {
-            if (processStructure.length === 0) callback(null,null);
+            if (processStructure.length === 0) callback(null, null);
             else callback(null, processStructure[0]._doc);
         }
     });
@@ -28,18 +28,17 @@ exports.getActiveProcessByProcessName = function (processName, callback) {
     ActiveProcess.find({process_name: processName}, (err, process) => {
         if (err) callback(err);
         else {
-            if (process.length === 0) callback(null,null);
-            else callback(null,process[0]);
-}
+            if (process.length === 0) callback(null, null);
+            else callback(null, process[0]);
+        }
     });
 };
 exports.getUsernameByRoleID = (roleID, callback) => {
     UsersAndRole.find({_id: roleID}, (err1, res) => {
         if (err1) {
             callback(err1);
-        }
-        else {
-            callback(null,res[0]._doc.userEmail[0]);
+        } else {
+            callback(null, res[0]._doc.userEmail[0]);
         }
     })
 };
