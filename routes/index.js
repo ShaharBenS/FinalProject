e52d370bb44e0aadd91d58d0cf0ed8728c5cf1c6
Omri <omri206attiya@gmail.com>
2019-03-01 12:00:09 +0200

    added a todo for change host name when we have one

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,5 @@ let router = express.Router();
 router.get('/', function (req, res) {
     res.redirect('http://localhost:3000/auth/google');
 });
-
+//TODO: change localhost:3000 to the real host name.
 module.exports = router;
