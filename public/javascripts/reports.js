f74aaa50bc3a59d32218f994bb8e4bc40ac7c86e
Tomer Lev <levtom@post.bgu.ac.il>
2019-03-02 14:34:16 +0200

    Working On Process Report.

--- public/javascripts/reports.js
+++ public/javascripts/reports.js
@@ -1,3 +1,3 @@
 function generateSummary(processName) {
-    window.location.href = '/reportsViews/processReport/?name=' + processName;
+    window.location.href = 'reportProcess/?process_name=' + processName;
 }
