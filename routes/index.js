ee6fdc4d1882fc4d171ec86e542fb722c0958e41
Tomer Lev <levtom@post.bgu.ac.il>
2019-03-03 14:31:39 +0200

    Fresh New Login Mechanism.

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,13 @@ let router = express.Router();
 
 /* GET home page. */
 
-router.get('/', ensureAuthenticated, function (req, res, next) {
-     res.render('index');
+router.get('/', function (req, res) {
+    res.render('userViews/login');
 });
 
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        res.render('index');
-    }
-    else
-    {
-        res.redirect('/auth/google')
-    }
-}
+router.get('/Home', function (req, res, next) {
+    res.render('index');
+});
+
+
 module.exports = router;
--- routes/usersRouters/auth.js
+++ routes/usersRouters/auth.js
@@ -2,27 +2,11 @@ let express = require('express');
 let router = express.Router();
 let passportGoogle = require('../../auth/google');
 
-/* LOGIN ROUTER */
-router.get('/login', function (req, res, next)
-{
-    res.render('login', {title: 'Please Sign In with:'});
-});
-
 /* LOGOUT ROUTER */
 router.get('/logout', function (req, res)
 {
-    console.log('1111111111111111');
-    console.log("logging out");
-    console.log(req.user.emails[0].value);
-    console.log(req.isAuthenticated());
-    console.log('1111111111111111');
     req.logout();
     res.redirect('/');
-    console.log('2222222222222222');
-    console.log("logging out");
-    //console.log(req.user.emails[0].value);
-    console.log(req.isAuthenticated());
-    console.log('2222222222222222');
 });
 
 /* GOOGLE ROUTER */
@@ -33,7 +17,13 @@ router.get('/google/callback',
     passportGoogle.authenticate('google', {failureRedirect: '/NotAgudaEmployee'}),
     function (req, res)
     {
-        res.redirect('/');
+        if (req.isAuthenticated()) {
+            res.redirect('/Home')
+        }
+        else
+        {
+            res.redirect('userViews/login')
+        }
     });
 
 module.exports = router;
