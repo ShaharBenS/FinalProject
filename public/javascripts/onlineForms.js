4273e48872824aab1faefd0788dbe432681f8400
Omri <omri206attiya@gmail.com>
2019-03-18 00:28:23 +0200

    modified forms to have setup function instead embedded script

--- public/javascripts/onlineForms.js
+++ public/javascripts/onlineForms.js
@@ -20,3 +20,37 @@ let submitForm = function () {
     window.close();
     return false;
 };
+
+
+let setupInputs = function (formName, isForShow, fields) {
+    if (fields !== 'false') {
+        while (fields.indexOf('&#34;') >= 0)
+            fields = fields.replace('&#34;', '"');
+        fields = JSON.parse(fields);
+        fields.forEach((field) => {
+            let element = document.getElementsByName(field.fieldName)[0];
+            if (element.type === 'text')
+                element.setAttribute("value", field.value);
+            else if (element.type === 'checkbox')
+                element.checked = true;
+            else if (element.type === 'radio') {
+                document.getElementsByName(field.fieldName).forEach((radioElement) => {
+                    if (radioElement.value === field.value) {
+                        radioElement.checked = true;
+                    }
+                    return true;
+                });
+            } else
+                alert('type error');
+        });
+        document.getElementById("fieldset").setAttribute("disabled", "disabled");
+        document.getElementById("submitButton").setAttribute("disabled", "disabled");
+        document.getElementById("submitButton").style.display = "none";
+    } else if (isForShow) {
+        let info = "טופס דמו של " + formName;
+        document.getElementById("info").appendChild(document.createTextNode(info));
+        document.getElementById("fieldset").setAttribute("disabled", "disabled");
+        document.getElementById("submitButton").setAttribute("disabled", "disabled");
+    } else
+        document.getElementById('close_win_button').style.display = "none";
+};
