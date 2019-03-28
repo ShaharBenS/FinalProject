281c14f0d318201bd59cfed59079b41bca349b07
Shahar <shahar0897@gmail.com>
2019-03-28 19:39:11 +0200

    Revert "fix color of the site, now saved in cookies"
    This reverts commit b472b344
    
    permissions control on GUI

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let router = express.Router();
 let usersAndRolesController = require('../controllers/usersControllers/usersAndRolesController');
-
+let usersPermissionsController = require('../controllers/usersControllers/UsersPermissionsController');
 /* GET home page. */
 
 router.get('/', function (req, res)
@@ -12,21 +12,32 @@ router.get('/', function (req, res)
 router.get('/getTopBar', function (req, res)
 {
     if (req.isAuthenticated()) {
-        usersAndRolesController.getRoleNameByUsername(req.user.emails[0].value, (err, roleName) =>
-        {
-            if (err) {
-                res.render('topbar', {roleName: "RoleNotFound", userFullName: ''});
-            }
-            else {
-                usersAndRolesController.getFullNameByEmail(req.user.emails[0].value,(err,fullName)=>{
-                    if(err){
-                        res.render('topbar', {roleName: roleName, userFullName: 'FullNameNotFound'});
-                    }
-                    else{
-                        res.render('topbar', {roleName: roleName, userFullName: fullName});
-                    }
-                });
-            }
+        usersPermissionsController.getUserPermissions(req.user.emails[0].value,(err,permission)=>{
+           if(err) {
+               res.send(err);
+           }
+           else{
+               let permissions = "style='display: none'";
+               if(permission.permissionsManagementPermission){
+                   permissions = "style='display: flex'";
+               }
+               usersAndRolesController.getRoleNameByUsername(req.user.emails[0].value, (err, roleName) =>
+               {
+                   if (err) {
+                       res.render('topbar', {roleName: "RoleNotFound", userFullName: '',permissionsStyle:permissions});
+                   }
+                   else {
+                       usersAndRolesController.getFullNameByEmail(req.user.emails[0].value,(err,fullName)=>{
+                           if(err){
+                               res.render('topbar', {roleName: roleName, userFullName: 'FullNameNotFound',permissionsStyle:permissions});
+                           }
+                           else{
+                               res.render('topbar', {roleName: roleName, userFullName: fullName,permissionsStyle:permissions});
+                           }
+                       });
+                   }
+               });
+           }
         });
     }
     else
