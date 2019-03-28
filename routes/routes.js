11334346c3dd9a816bf15f58f9ab3c2a6da9dcbd
Tomer Lev <levtom@post.bgu.ac.il>
2019-03-28 12:40:52 +0200

    Prevent unautorized access to inside urls.

--- routes/routes.js
+++ routes/routes.js
@@ -14,18 +14,22 @@ let userPermissionsControl = require('./usersRouters/permissionsControlRouter');
 
 let onlineFormsRouter = require('./onlineFormsRoutes/onlineFormsRouter');
 
-module.exports = (app)=>{
+module.exports = (app) => {
     app.use('/', indexRouter);
+    app.use('/auth', auth);
+    app.all('*', function (req, res, next) {
+        if (req.isAuthenticated()) {
+            next();
+        } else {
+            res.redirect('/');
+        }
+    });
     app.use('/sankey', sankeyRouter);
-
     app.use('/activeProcesses', activeProcessesRouter);
-
     app.use('/permissionErrors', permissionErrorsRouter);
-
     app.use('/processStructures', processStructuresRouter);
     app.use('/notifications', processNotificationRouter);
     app.use('/usersAndRoles', usersAndRolesRouter);
-    app.use('/auth', auth);
     app.use('/uploadFile', uploadFilesRouter);
     app.use('/permissionsControl', userPermissionsControl);
     app.use('/uploadFile', uploadFilesRouter);
