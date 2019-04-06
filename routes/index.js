5a6fc9e4e8ea2c8b53b4b87bdcbf472a0d3bd802
Shahar <shahar0897@gmail.com>
2019-04-06 13:49:22 +0300

    - added admins schema - updated top bar if admin - first one to create the tree will be an admin - admins must have all permissions

--- routes/index.js
+++ routes/index.js
@@ -14,43 +14,81 @@ router.get('/', function (req, res)
 router.get('/getTopBar', function (req, res)
 {
     if (req.isAuthenticated()) {
-        usersPermissionsController.getUserPermissions(req.user.emails[0].value,(err,permission)=>{
-           if(err) {
-               res.send(err);
-           }
-           else{
-               let permissions = "style='display: none'";
-               if(permission.permissionsManagementPermission){
-                   permissions = "style='display: flex'";
-               }
-               usersAndRolesController.getRoleNameByUsername(req.user.emails[0].value, (err, roleName) =>
-               {
-                   if (err) {
-                       res.render('topbar', {roleName: "RoleNotFound", userFullName: '',permissionsStyle:permissions, notificationCount: ''});
-                   }
-                   else {
-                       usersAndRolesController.getFullNameByEmail(req.user.emails[0].value,(err,fullName)=>{
+        usersPermissionsController.getUserPermissions(req.user.emails[0].value, (err, permission) =>
+        {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                let permissions = "style='display: none'";
+                if (permission.permissionsManagementPermission) {
+                    permissions = "style='display: flex'";
+                }
+                usersAndRolesController.getRoleNameByUsername(req.user.emails[0].value, (err, roleName) =>
+                {
+                    if (err) {
+                        usersAndRolesController.findAdmins((err,admins) =>
+                        {
                            if(err){
-                               res.render('topbar', {roleName: roleName, userFullName: 'FullNameNotFound',permissionsStyle:permissions, notificationCount:''});
-                           }
-                           else{
-                               notificationController.countNotifications(req.user.emails[0].value,(err,count)=>{
-                                   if(err){
-                                       res.send(err);
-                                   }
-                                   else{
-                                       res.render('topbar', {roleName: roleName, userFullName: fullName,permissionsStyle:permissions, notificationCount:count === 0 ? "" : count});
-                                   }
+                               res.render('topbar', {
+                                   roleName: "RoleNotFound",
+                                   userFullName: '',
+                                   permissionsStyle: permissions,
+                                   notificationCount: ''
                                });
                            }
-                       });
-                   }
-               });
-           }
+                           else if(admins.includes(req.user.emails[0].value)){
+                               res.render('topbar', {
+                                   roleName: "Admin",
+                                   userFullName: 'אדמין',
+                                   permissionsStyle: permissions,
+                                   notificationCount: ''
+                               });
+                           }
+                           else{
+                               res.render('topbar', {
+                                   roleName: "RoleNotFound",
+                                   userFullName: '',
+                                   permissionsStyle: permissions,
+                                   notificationCount: ''
+                               });
+                           }
+                        });
+                    }
+                    else {
+                        usersAndRolesController.getFullNameByEmail(req.user.emails[0].value, (err, fullName) =>
+                        {
+                            if (err) {
+                                res.render('topbar', {
+                                    roleName: roleName,
+                                    userFullName: 'FullNameNotFound',
+                                    permissionsStyle: permissions,
+                                    notificationCount: ''
+                                });
+                            }
+                            else {
+                                notificationController.countNotifications(req.user.emails[0].value, (err, count) =>
+                                {
+                                    if (err) {
+                                        res.send(err);
+                                    }
+                                    else {
+                                        res.render('topbar', {
+                                            roleName: roleName,
+                                            userFullName: fullName,
+                                            permissionsStyle: permissions,
+                                            notificationCount: count === 0 ? "" : count
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
         });
     }
-    else
-    {
+    else {
         res.redirect('/')
     }
 });
@@ -60,8 +98,7 @@ router.get('/Home', function (req, res)
     if (req.isAuthenticated()) {
         res.render('index')
     }
-    else
-    {
+    else {
         res.redirect('/')
     }
 });
