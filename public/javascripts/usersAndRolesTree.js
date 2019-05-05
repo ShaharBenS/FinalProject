72bf0e4263165782e08c64b5f16f970bec97e22e
Shahar <shahar0897@gmail.com>
2019-05-05 10:28:11 +0300

    - added warnings and changed names

--- public/javascripts/usersAndRolesTree.js
+++ public/javascripts/usersAndRolesTree.js
@@ -1,4 +1,5 @@
-let add_label = function () {};
+let add_label = function () {
+};
 
 let roleToEmails = {};     // roleName to usersEmail
 let idToRole = {};
@@ -6,9 +7,8 @@ let emailToFullName = {};
 let roleToDereg = {};
 
 var xmlHttp = new XMLHttpRequest();
-xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
-    {
+xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
         roleToEmails = JSON.parse(xmlHttp.responseText)
     }
 };
@@ -16,9 +16,8 @@ xmlHttp.open("GET", '/usersAndRoles/getRoleToEmails/', true);
 xmlHttp.send(null);
 
 var xmlHttp1 = new XMLHttpRequest();
-xmlHttp1.onreadystatechange = function() {
-    if (xmlHttp1.readyState === 4 && xmlHttp1.status === 200)
-    {
+xmlHttp1.onreadystatechange = function () {
+    if (xmlHttp1.readyState === 4 && xmlHttp1.status === 200) {
         emailToFullName = JSON.parse(xmlHttp1.responseText)
     }
 };
@@ -26,9 +25,8 @@ xmlHttp1.open("GET", '/usersAndRoles/getEmailToFullName/', true);
 xmlHttp1.send(null);
 
 var xmlHttp2 = new XMLHttpRequest();
-xmlHttp2.onreadystatechange = function() {
-    if (xmlHttp2.readyState === 4 && xmlHttp2.status === 200)
-    {
+xmlHttp2.onreadystatechange = function () {
+    if (xmlHttp2.readyState === 4 && xmlHttp2.status === 200) {
         roleToDereg = JSON.parse(xmlHttp2.responseText)
     }
 };
@@ -36,12 +34,11 @@ xmlHttp2.open("GET", '/usersAndRoles/getRoleToDereg/', true);
 xmlHttp2.send(null);
 
 
-$(document).ready(()=>{
+$(document).ready(() => {
     var modal = document.getElementById('select_users_modal');
     var modal2 = document.getElementById('select-dereg-modal');
 
-    window.onclick = function(event)
-    {
+    window.onclick = function (event) {
         if (event.target === modal) {
             modal.style.display = "none";
         }
@@ -51,9 +48,8 @@ $(document).ready(()=>{
     };
 
     var xmlHttp2 = new XMLHttpRequest();
-    xmlHttp2.onreadystatechange = function() {
-        if (xmlHttp2.readyState === 4 && xmlHttp2.status === 200)
-        {
+    xmlHttp2.onreadystatechange = function () {
+        if (xmlHttp2.readyState === 4 && xmlHttp2.status === 200) {
             idToRole = JSON.parse(xmlHttp2.responseText);
         }
     };
@@ -70,17 +66,16 @@ function getHelp() {
         '3. הוספת אנשים לתפקיד מסויים מתבצעת על עדע לחיצה ימנית על התפקיד, בחירה באפשרות ראה משתמשים ולאחר מכן לחיצה על לחצן הפלוס והזנת כתובת המייל של העובד ושמו.'
         + '\n' +
         '4. הגדרת הקשר בין התפקידים תתבצע על ידי מתיחת חץ מהתפקיד הבכיר (האב) אל התפקיד הזוטר (הבן). (מתיחת הקשרים הינה משמאל לימין)';
-    alertify.alert(head,stringOfGuide);
+    alertify.alert(head, stringOfGuide);
 }
 
 function onDrop_extension(type, command, figure) {
-    alertify.prompt("הכנס את שם התפקיד","",(evt,role_name)=>{
-        if(role_name != null){
-            if(roleToEmails[role_name] === undefined){
-                if(role_name === ""){
+    alertify.prompt("הכנס את שם התפקיד", "", (evt, role_name) => {
+        if (role_name != null) {
+            if (roleToEmails[role_name] === undefined) {
+                if (role_name === "") {
                     alertify.alert('שם תפקיד לא יכול להיות ריק');
-                }
-                else{
+                } else {
                     roleToDereg[role_name] = "1";
                     idToRole[figure.id] = role_name;
                     roleToEmails[role_name] = [];
@@ -100,10 +95,9 @@ function onDrop_extension(type, command, figure) {
                     figure.setBackgroundColor("f6a500");
                     app.view.getCommandStack().execute(command);
                     figure.setWidth(Math.max(figure.getWidth(), figure.label.getWidth()));
-                    figure.setHeight(figure.height+30);
+                    figure.setHeight(figure.height + 30);
                 }
-            }
-            else{
+            } else {
                 alertify.alert('שם תפקיד זה כבר קיים');
             }
         }
@@ -112,18 +106,16 @@ function onDrop_extension(type, command, figure) {
 
 let currentRoleNameClicked = '';
 
-function changeDeregClicked(){
+function changeDeregClicked() {
     let selector = document.getElementById('dereg-select');
     roleToDereg[currentRoleNameClicked] = selector.options[selector.selectedIndex].value;
     document.getElementById('select-dereg-modal').style.display = 'none';
 }
 
-function rolesToHTML(roleName)
-{
+function rolesToHTML(roleName) {
     let users_div = document.getElementById("users-div");
     users_div.innerHTML = '';
-    roleToEmails[roleName].forEach((userEmail) =>
-    {
+    roleToEmails[roleName].forEach((userEmail) => {
         let outerDiv = document.createElement("div");
         outerDiv.style.display = "flex";
         outerDiv.style.flexDirection = "row";
@@ -138,10 +130,10 @@ function rolesToHTML(roleName)
         let a = document.createElement("a");
         a.innerHTML = '<i class="ion ion-android-remove-circle" style="color:var(--main-color) ;font-size: 30px"></i>';
         a.style.marginRight = "10px";
-        a.onclick = ()=>{
+        a.onclick = () => {
             let index = roleToEmails[roleName].indexOf(userEmail);
-            if(index > -1){
-                roleToEmails[roleName].splice(index,1);
+            if (index > -1) {
+                roleToEmails[roleName].splice(index, 1);
                 delete emailToFullName[userEmail];
             }
             rolesToHTML(roleName);
@@ -170,32 +162,31 @@ function rolesToHTML(roleName)
     div.style.justifyContent = "center";
     let a = document.createElement("a");
     a.innerHTML = '<i class="ion ion-android-add-circle" style="color:var(--main-color);font-size: 30px"></i>';
-    a.onclick = () =>
-    {
-        alertify.prompt("הכנס אימייל:","",(evt,email)=>{
+    a.onclick = () => {
+        alertify.prompt("הכנס אימייל:", "", (evt, email) => {
             if (email != null) {
-                if(!emailValidator(email)){
+                if (!emailValidator(email)) {
                     alertify.alert("כתובת מייל לא תקינה");
                     return;
                 }
                 let found = false;
-                Object.keys(roleToEmails).forEach(roleName=>{
-                    roleToEmails[roleName].forEach(userEmail=>{
-                        if(email === userEmail){
+                Object.keys(roleToEmails).forEach(roleName => {
+                    roleToEmails[roleName].forEach(userEmail => {
+                        if (email === userEmail) {
                             found = true;
-                            alertify.alert('המייל כבר בשימוש, בתפקיד: '+roleName)
+                            alertify.alert('המייל כבר בשימוש, בתפקיד: ' + roleName)
                         }
                     })
                 });
-                if(!found){
-                    setTimeout(()=>{
-                        alertify.prompt("הכנס שם מלא:","",(evt,fullName)=>{
+                if (!found) {
+                    setTimeout(() => {
+                        alertify.prompt("הכנס שם מלא:", "", (evt, fullName) => {
                             updateUsersMaps(roleName);
                             emailToFullName[email] = fullName;
                             roleToEmails[roleName].push(email);
                             rolesToHTML(roleName);
                         });
-                    },400);
+                    }, 400);
                 }
             }
         });
@@ -205,69 +196,71 @@ function rolesToHTML(roleName)
     users_div.append(div);
 }
 
-function updateUsersMaps(roleName){
+function updateUsersMaps(roleName) {
     let emails = document.getElementsByClassName("email");
     let names = document.getElementsByClassName("name");
     let emailsArray = [];
-    for(let i = 0; i < emails.length; i++){
-        if(!emailValidator(emails[i].value)){
+    for (let i = 0; i < emails.length; i++) {
+        if (!emailValidator(emails[i].value)) {
             return emails[i].value;
         }
         emailsArray.push(emails[i].value);
     }
-    for(let i = 0; i < emailsArray.length; i++){
+    for (let i = 0; i < emailsArray.length; i++) {
         emailToFullName[emailsArray[i]] = names[i].value;
     }
     roleToEmails[roleName] = emailsArray;
     return undefined;
 }
 
-function updateUsername(){
+function updateUsername() {
     let result = updateUsersMaps(currentRoleNameClicked);
-    if(result === undefined){
+    if (result === undefined) {
         document.getElementById('select_users_modal').style.display = 'none';
-    }
-    else{
-        alertify.alert("הכתובת "+result+" אינה תקינה.");
+    } else {
+        alertify.alert("הכתובת " + result + " אינה תקינה.");
     }
 }
 
-function deleteRoleById(id){
+function deleteRoleById(id) {
     let emails = roleToEmails[idToRole[id]];
-    emails.forEach(email=>{
-       delete emailToFullName[email];
+    emails.forEach(email => {
+        delete emailToFullName[email];
     });
     delete roleToEmails[idToRole[id]];
     delete roleToDereg[idToRole[id]];
     delete idToRole[id];
 }
 
-function emailValidator(email)
-{
+function emailValidator(email) {
     let regularExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regularExpression.test(String(email).toLowerCase());
 }
 
-function loadDefaultTree(){
-    $.ajax({
-            url: '/usersAndRoles/loadDefaultTree/',
-            method: "POST",
-            xhrFields: {
-                withCredentials: true
-            },
-            data: {},
+function loadDefaultTree() {
+    alertify.confirm('', 'האם אתה בטוח שאתה רוצה לטעון את עץ ברירת המחדל?', function () {
+            $.ajax({
+                    url: '/usersAndRoles/loadDefaultTree/',
+                    method: "POST",
+                    xhrFields: {
+                        withCredentials: true
+                    },
+                    data: {},
+                }
+            ).done(function (responseText, status) {
+                if (status === "success") {
+                    if (responseText === "success") {
+                        alertify.alert("העץ נשמר בהצלחה!", () => {
+                            window.location.href = '/usersAndRoles/editTree/';
+                        });
+                    } else {
+                        alertify.alert(responseText);
+                    }
+                }
+            });
         }
-    ).done(function (responseText, status) {
-        if (status === "success") {
-            if (responseText === "success") {
-                alertify.alert("העץ נשמר בהצלחה!",()=>{
-                    window.location.href = '/usersAndRoles/editTree/';
-                });
-            } else {
-                alertify.alert(responseText);
-            }
-        }
-    });
+        , function () {
+        });
 }
 
 function confirm() {
