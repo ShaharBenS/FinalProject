81c48e21397171ff71a864ab28f8f789c5faa54e
Tomer Lev <levtom@post.bgu.ac.il>
2019-05-08 09:58:45 +0300

    Fix The Gui Test On Master.

--- test/UI_Tests/firstSketch.js
+++ test/UI_Tests/firstSketch.js
@@ -5,7 +5,7 @@ let ClientFunction = require("testcafe").ClientFunction;
 let getCurrentUrl = ClientFunction(() => window.location.href);
 
 fixture('Login Test')
-    .page('https://localhost:3000/');
+    .page('https://localhost');
 
 test('Create todo', async browser => {
     await browser
@@ -16,7 +16,7 @@ test('Create todo', async browser => {
     await browser
         .typeText('[name="passwd"]', 'tomer8108')
         .pressKey('enter');
-    await browser.expect(getCurrentUrl()).eql('https://localhost:3000/Home', {timeout: 5000});
+    await browser.expect(getCurrentUrl()).eql('https://localhost/Home', {timeout: 5000});
 });
 
 /*
