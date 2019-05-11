600447c844747585041e947c6508ed54c71a1aaa
Kuti1997 <kutigolberg@gmail.com>
2019-05-11 13:30:23 +0300

    Gui Test Continue

--- test/UI_Tests/ActiveProcessStartAndHandle_UI_Test.js
+++ test/UI_Tests/ActiveProcessStartAndHandle_UI_Test.js
@@ -8,41 +8,49 @@ let rolesToDereg = require('../inputs/trees/treeForGUIStartAndHandle/rolesToDere
 let rolesToEmails = require('../inputs/trees/treeForGUIStartAndHandle/rolesToEmails');
 let processStructureSankeyJSON = require('../inputs/processStructures/processStructureForGuiStartAndHandle/processStructure');
 
-
-let beforeGlobal = function (done) {
-    userAccessor.createSankeyTree({sankey: JSON.stringify({content: {diagram: []}})}, (err, result) => {
-        if (err) {
-            console.log('blah blah');
-            done(err);
-        }
-        else {
-            console.log('blah blah2');
-            UsersAndRolesTreeSankey.setUsersAndRolesTree('yor@outlook.co.il', JSON.stringify(sankeyContent),
-                rolesToEmails, emailsToFullName,
-                rolesToDereg, (err) => {
-                    if (err) {
-                        done(err);
-                    }
-                    else {
-                        processStructureController.addProcessStructure('yor@outlook.co.il', 'תהליך אישור', JSON.stringify(processStructureSankeyJSON), [], 0, "12", (err, needApproval) => {
-                            if (err) {
-                                done(err);
-                            }
-                            else {
-                                done();
-                            }
-                        });
-                    }
-                });
-        }
+function insertToDB() {
+    return new Promise(resolve => {
+        userAccessor.createSankeyTree({sankey: JSON.stringify({content: {diagram: []}})}, (err, result) => {
+            console.log('blahblah');
+            if (err) {
+                resolve(err);
+            }
+            else {
+                UsersAndRolesTreeSankey.setUsersAndRolesTree('yor@outlook.co.il', JSON.stringify(sankeyContent),
+                    rolesToEmails, emailsToFullName,
+                    rolesToDereg, (err) => {
+                        if (err) {
+                            resolve(err);
+                        }
+                        else {
+                            processStructureController.addProcessStructure('yor@outlook.co.il', 'תהליך אישור', JSON.stringify(processStructureSankeyJSON), [], 0, "12", (err, needApproval) => {
+                                if (err) {
+                                    resolve(err);
+                                }
+                                else {
+                                    resolve();
+                                }
+                            });
+                        }
+                    });
+            }
+        });
     });
+}
+
+let beforeGlobal = async function () {
+    return await insertToDB();
 };
 
 let getCurrentUrl = ClientFunction(() => window.location.href);
 
-fixture('Handle Process').page('https://localhost:3000/').before(beforeGlobal);
+fixture('Handle Process').page('https://localhost:3000/');
 
 test('Start And Handle', async browser => {
+    /*this.enableTimeouts(false);
+    mongoose.set('useCreateIndex', true);
+    await mongoose.connect('mongodb://localhost:27017/Tests', {useNewUrlParser: true});
+    let something = await insertToDB();*/
     await browser.setNativeDialogHandler(() => true);
     await browser
         .click('#login_button');
@@ -199,6 +207,84 @@ test('Handle', async browser => {
         .click('[id="תהליך אישור"]');
     await browser
         .typeText('[name="comments"]', 'הערות של גרפיקאי')
+        .click('#advanceProcess');
+    /*const history = await browser.getNativeDialogHistory();
+    await browser
+        .expect(history.length).eql(1)
+        .expect(history[0].type).eql('alert')
+        .expect(history[0].text).eql('שם לא יכול להיות ריק');*/
+    /*await browser
+    await browser.expect(getCurrentUrl()).eql('https://localhost:3000/Home', {timeout: 5000});*/
+});
+
+test('Return To Creator', async browser => {
+    await browser.setNativeDialogHandler(() => true);
+    await browser
+        .click('#login_button');
+    await browser
+        .typeText('[name="loginfmt"]', 'kutigolberg@outlook.co.il')
+        .pressKey('enter');
+    await browser
+        .typeText('[name="passwd"]', 'tomer8108')
+        .pressKey('enter');
+    await browser
+        .click('[name="myWaitingProcesses"]');
+    await browser
+        .click('[id="תהליך אישור"]');
+    await browser
+        .typeText('[name="comments"]', 'הערות של רמד הסברה')
+        .click('#returnToCreator1');
+    /*const history = await browser.getNativeDialogHistory();
+    await browser
+        .expect(history.length).eql(1)
+        .expect(history[0].type).eql('alert')
+        .expect(history[0].text).eql('שם לא יכול להיות ריק');*/
+    /*await browser
+    await browser.expect(getCurrentUrl()).eql('https://localhost:3000/Home', {timeout: 5000});*/
+});
+
+test('Handle', async browser => {
+    await browser.setNativeDialogHandler(() => true);
+    await browser
+        .click('#login_button');
+    await browser
+        .typeText('[name="loginfmt"]', 'levtom@outlook.co.il')
+        .pressKey('enter');
+    await browser
+        .typeText('[name="passwd"]', 'tomer8108')
+        .pressKey('enter');
+    await browser
+        .click('[name="myWaitingProcesses"]');
+    await browser
+        .click('[id="תהליך אישור"]');
+    await browser
+        .typeText('[name="comments"]', 'הערות של אחראי מיתוג קמפיינים')
+        .click('#advanceProcess');
+    /*const history = await browser.getNativeDialogHistory();
+    await browser
+        .expect(history.length).eql(1)
+        .expect(history[0].type).eql('alert')
+        .expect(history[0].text).eql('שם לא יכול להיות ריק');*/
+    /*await browser
+    await browser.expect(getCurrentUrl()).eql('https://localhost:3000/Home', {timeout: 5000});*/
+});
+
+test('Handle', async browser => {
+    await browser.setNativeDialogHandler(() => true);
+    await browser
+        .click('#login_button');
+    await browser
+        .typeText('[name="loginfmt"]', 'levtom2@outlook.co.il')
+        .pressKey('enter');
+    await browser
+        .typeText('[name="passwd"]', 'tomer8108')
+        .pressKey('enter');
+    await browser
+        .click('[name="myWaitingProcesses"]');
+    await browser
+        .click('[id="תהליך אישור"]');
+    await browser
+        .typeText('[name="comments"]', 'הערות של אחראי רכש')
         .click('#advanceProcess');
     /*const history = await browser.getNativeDialogHistory();
     await browser
