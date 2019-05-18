cc6accf96d7edfabf9739d5fb1979680ac82ee7c
Tomer Lev <levtom@post.bgu.ac.il>
2019-05-18 12:03:25 +0300

    Add Untake Functionallity.

---
+++ public/javascripts/untakePartInProcess.js
@@ -0,0 +1,23 @@
+function takePartInProcess(processName,userEmail)
+{
+    let xhr = new XMLHttpRequest();
+    let data = new FormData();
+    data.append('processName', processName);
+    data.append('userEmail', userEmail);
+    xhr.open("POST", '/activeProcesses/unTakePartInProcess', true);
+    //xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() { // Call a function when the state changes.
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            if(xhr.responseText === "success")
+            {
+                alert('החזרת תהליך למאגר התהליכים הזמינים הצליחה.');
+            }
+            else
+            {
+                alert('קרתה שגיאה בעת החזרת תהליך למאגר התהליכים הזמינים.');
+            }
+            window.location.href = "/activeProcesses/getWaitingActiveProcessesByUser";
+        }
+    };
+    xhr.send(data);
+}
